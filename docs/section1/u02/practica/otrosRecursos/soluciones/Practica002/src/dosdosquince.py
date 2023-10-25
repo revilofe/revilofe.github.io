58087c33b56f920b1cf4a832ad0973eaace0a570
Edu <revilofe@gmail.com>
2023-10-25 08:45:04 +0200

    Rebase ejercicios

--- docs/section1/u02/practica/otrosRecursos/soluciones/Practica002/src/dosdosquince.py
+++ docs/section1/u02/practica/otrosRecursos/soluciones/Practica002/src/dosdosquince.py
@@ -1,52 +1,49 @@
-''' **Ejercicio 2.2.15**
-Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la sumatoria de todos los números positivos ingresados.
+""" **Ejercicio 2.2.15** Leer números enteros de teclado, hasta que el usuario ingrese el 0. Finalmente, mostrar la
+sumatoria de todos los números positivos ingresados.
 
-# 
 
-sumatoria = 0
-numero = int(input("Ingrese un número entero: "))
+"""
 
-while numero != 0:
-    sumatoria += numero
-    ynumero = int(input("Ingrese otro número entero (o 0 para salir): "))
 
-print("La sumatoria de los números ingresados es:", sumatoria)
-
-'''
-
-def leerNumerosHastaLeerCero() -> list:
-    ''' Lee números enteros de teclado, hasta que el usuario ingrese el 0. 
+def leer_numeros_hasta_leer_cero() -> list:
+    """ Lee números enteros de teclado, hasta que el usuario ingrese el 0.
         Devuelve una lista con los números ingresados.
-    '''
-    numeros = []
+    """
+    numeros_leidos = []
     numero = int(input("Ingrese un número entero: "))
     while numero != 0:
-        numeros.append(numero)
+        numeros_leidos.append(numero)
         numero = int(input("Ingrese otro número entero (o 0 para salir): "))
-    return numeros
+    return numeros_leidos
 
-def sumatoriaDePositivos(numeros: list) -> int:
-    ''' Devuelve la sumatoria de todos los números de la lista.
-    '''
+
+def sumatoria_de_positivos(numeros_a_sumar: list) -> int:
+    """ Devuelve la sumatoria de todos los números de la lista.
+    """
     suma = 0
-    for numero in numeros:
+    for numero in numeros_a_sumar:
         if numero > 0:
             suma += numero
     return suma
 
-def construyeMensaje(total: int ) -> str:
-    ''' Construye el mensaje de salida.
-    '''
-    return "La sumatoria de los números ingresados es: " + str(total)
+
+def construye_mensaje(total_sumatoria: int) -> str:
+    """ Construye el mensaje de salida.
+    """
+    return "La sumatoria de los números ingresados es: " + str(total_sumatoria)
+
 
 # Bloque principal del programa
-if __name__ == "__main__":    
+if __name__ == "__main__":
     # Entrada
-    numeros = leerNumerosHastaLeerCero()
+    numeros = leer_numeros_hasta_leer_cero()
 
     # Proceso
-    total = sumatoriaDePositivos(numeros)
-    mensaje = construyeMensaje(total)
+    total = sumatoria_de_positivos(numeros)
+    mensaje = construye_mensaje(total)
 
     # Salida
     print(mensaje)
+
+
+

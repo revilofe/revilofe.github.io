4b24a14b8f7142495aa6728829d0073e00b4b682
Edu <revilofe@gmail.com>
2023-10-26 18:09:06 +0200

    ejercicio 2.2.20. refactor

--- docs/section1/u02/practica/otrosRecursos/soluciones/Practica002/src/dosdosveinte.py
+++ docs/section1/u02/practica/otrosRecursos/soluciones/Practica002/src/dosdosveinte.py
@@ -1,6 +1,7 @@
-"""
-Ejercicio 2.2.20
-Solicitar al usuario el ingreso de una frase y de una letra (que puede o no estar en la frase). Recorrer la frase, carácter a carácter, comparando con la letra buscada. Si el carácter no coincide, indicar que no hay coincidencia en esa posición (imprimiendo la posición) y continuar. Si se encuentra una coincidencia, indicar en qué posición se encontró y finalizar la ejecución.
+"""Ejercicio 2.2.20 Solicitar al usuario el ingreso de una frase y de una letra (que puede o no estar en la frase).
+Recorrer la frase, carácter a carácter, comparando con la letra buscada. Si el carácter no coincide, indicar que no
+hay coincidencia en esa posición (imprimiendo la posición) y continuar. Si se encuentra una coincidencia, indicar en
+qué posición se encontró y finalizar la ejecución.
 
 """
 
@@ -11,10 +12,10 @@ def leer_frase() -> str:
 
     :return: La frase leída.
     """
-    frase = input("Ingrese una frase, minimo 2 palabras: ").strip()
-    while len(frase.split()) < 2:
-        frase = input("Ingrese una frase, minimo 2 palabras: ").strip()
-    return frase
+    frase_leida = input("Ingrese una frase, minimo 2 palabras: ").strip()
+    while len(frase_leida.split()) < 2:
+        frase_leida = input("Ingrese una frase, minimo 2 palabras: ").strip()
+    return frase_leida
 
 
 def leer_letra() -> str:
@@ -23,29 +24,39 @@ def leer_letra() -> str:
 
     :return: La letra leída.
     """
-    letra = input("Ingrese una letra: ").strip()
-    while len(letra) != 1:
-        letra = input("Ingrese una letra: ").strip()
-    return letra
+    letra_leida = input("Ingrese una letra: ").strip()
+    while len(letra_leida) != 1:
+        letra_leida = input("Ingrese una letra: ").strip()
+    return letra_leida
 
 
-def mensaje_de_caracter_en_frase(letra: str, frase: str) -> str:
-    '''
-     Recorrer la frase, carácter a carácter, comparando con la letra buscada. Si el carácter no coincide, indicar que no hay coincidencia en esa posición (a añadiendo al mensaje final ese mensaje con la posición) y continuar. Si se encuentra una coincidencia, añadir al mensaje final en qué posición se encontró y devolver el mensaje completo.
-    '''
-
-    mensaje = ''
-
+def encontrar_posicion_de_caracter_en_frase(letra_buscada: str, frase_de_busqueda: str) -> int:
+    """
+    Recorrer la frase, carácter a carácter, comparando con la letra buscada.
+    Si encuentra el caracter, devolver la posición en la frase, el tamaño de la frase no encuentra el caracter.
+    """
     posicion_en_frase = 0
     no_encontrado = True
-    while posicion_en_frase < len(frase) and no_encontrado:
-        if frase[posicion_en_frase] == letra:
-            mensaje += f"La letra {letra} se encuentra en la posicion {posicion_en_frase}\n"
+    while posicion_en_frase < len(frase_de_busqueda) and no_encontrado:
+        if frase_de_busqueda[posicion_en_frase] == letra_buscada:
             no_encontrado = False
         else:
-            mensaje += f"La letra {letra} no se encuentra en la posicion {posicion_en_frase}\n"
-        posicion_en_frase += 1
-    return mensaje
+            posicion_en_frase += 1
+    return posicion_en_frase
+
+
+def mensaje_de_caracter_en_frase(posicion: int, longitud: int, letra_buscada: str) -> str:
+    """
+    Genera mensaje a sacar por pantalla.
+    """
+
+    mensaje_salida = ''
+    for posicion_en_frase in range(0, posicion + 1):
+        if posicion_en_frase == posicion and posicion_en_frase < longitud:
+            mensaje_salida += f"La letra {letra_buscada} se encuentra en la posicion {posicion_en_frase}\n"
+        elif posicion_en_frase < longitud:
+            mensaje_salida += f"La letra {letra_buscada} no se encuentra en la posicion {posicion_en_frase}\n"
+    return mensaje_salida
 
 
 if __name__ == "__main__":
@@ -54,7 +65,8 @@ if __name__ == "__main__":
     letra = leer_letra()
 
     # Procesamiento de datos
-    mensaje = mensaje_de_caracter_en_frase(letra, frase)
+    posicion_encontrada = encontrar_posicion_de_caracter_en_frase(letra, frase)
+    mensaje = mensaje_de_caracter_en_frase(posicion_encontrada, len(frase), letra)
 
     # Salida de datos
     print(mensaje)
